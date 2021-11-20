8669e5caed5fd345f59e1044600949f0ac13f3ce
Guilbert alves <guilbert.alves6162@gmail.com>
2021-11-20 17:20:44 -0500

    priemiro passo

---
+++ src/main/java/br/gov/ac/sefaz/resistendiaAPI/ResistendiaApiApplication.java
@@ -0,0 +1,13 @@
+package br.gov.ac.sefaz.resistendiaAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResistendiaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResistendiaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/gov/ac/sefaz/resistendiaAPI/ResistendiaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gov.ac.sefaz.resistendiaAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResistendiaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
